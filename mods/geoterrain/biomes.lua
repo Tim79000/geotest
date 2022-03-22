7433d22e9091a87e2a5c42fecd532ca217cfae63
Eric K Rustrum <rustrumek@gmail.com>
2022-03-22 13:20:52 -0700

    Set up stratum generation

--- mods/geoterrain/biomes.lua
+++ mods/geoterrain/biomes.lua
@@ -1,1 +1,14 @@
 minetest.register_alias("mapgen_stone", "geoterrain:chalk")
+minetest.register_alias("mapgen_bedrock", "geoterrain:granite")
+
+local add_stratum = function(y_min, y_max, node, biomes)
+	minetest.register_ore({
+		ore_type         = "stratum",
+		ore              = node,
+		wherein          = {"bakedclay:orange"},
+		clust_scarcity   = 1,
+		y_max            = y_max,
+		y_min            = y_min,
+		biomes = biomes
+	})
+end
