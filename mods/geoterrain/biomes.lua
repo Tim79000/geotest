628203cdf44e0161e320079121508f855d3a5c5b
MadKamel <rustrumek@gmail.com>
2022-03-22 13:58:14 -0700

    Hacky biome thing fix.
    :\

--- mods/geoterrain/biomes.lua
+++ mods/geoterrain/biomes.lua
@@ -2,13 +2,17 @@ minetest.register_alias("mapgen_stone", "geoterrain:chalk")
 minetest.register_alias("mapgen_bedrock", "geoterrain:granite")
 
 local add_stratum = function(y_min, y_max, node, biomes)
+	if not biomes then
+		biomes = minetest.get_registered_biomes --every single biome.
 	minetest.register_ore({
 		ore_type         = "stratum",
 		ore              = node,
-		wherein          = {"bakedclay:orange"},
+		wherein          = {"mapgen:stone"},
 		clust_scarcity   = 1,
 		y_max            = y_max,
 		y_min            = y_min,
 		biomes = biomes
 	})
 end
+
+add_stratum(-50, -25, "mapgen_bedrock", null)
